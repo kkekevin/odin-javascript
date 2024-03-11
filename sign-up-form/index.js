f311215b2158a42210362695e7143ae5c6305176
kkekevin <kke_kevin@hotmail.com>
2024-03-11 20:57:38 -0300

    add js to compare both passwords input

---
+++ sign-up-form/index.js
@@ -0,0 +1,12 @@
+let password = document.getElementsByName('password');
+let confirmPassword = document.getElementsByName('passwordConfirm');
+console.log(password);
+
+confirmPassword[0].addEventListener('change', () => {
+    console.log('aaa');
+    if (password[0].value !== '' && password[0].value != confirmPassword[0].value) {
+        document.getElementById('warningUnmatch').style.color = 'red';
+        document.getElementById('warningUnmatch').textContent = "* password do not match";
+    }
+});
+
