a3ccc6035ee93284654562203dced41635e5abd6
kkekevin <kke_kevin@hotmail.com>
2024-07-27 01:31:21 -0300

    Display initial messages

--- Mini message board/app.js
+++ Mini message board/app.js
@@ -1,12 +1,14 @@
 const express = require ('express');
 const app = express();
 const path = require ('path');
-const route = require ('routes/Message');
+const route = require ('./routes/index');
+const messageRoute = require ('./routes/message')
 
-//app.set('views', path.join(__dirname, 'views'));
-//app.set('view engine', 'ejs');
+app.use("/", route);
 
-app.get("/", route.get);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-  const PORT = 5000;
-  app.listen(PORT, () => console.log(`connected ${PORT}`));
+
+const PORT = 5000;
+app.listen(PORT, () => console.log(`connected ${PORT}`));
